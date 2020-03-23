53eb1e82a038f8e8464de59f31c14fb7b95ab0ff
seanaleid <seanaleid@gmail.com>
2020-03-23 13:36:08 -0400

    Add a new test with mock data
      * Mocks data to pass down via missions
      * Tests with no data and then rerenders to pass down mock data

--- src/components/MissionsList.js
+++ src/components/MissionsList.js
@@ -1,5 +1,30 @@
 import React from "react";
 
+// const MissionsList = props => {
+//   return (
+//     <section className="missions-list">
+//       {props.error ? (
+//         <div data-testid="error-message" className="error">
+//           {props.error}
+//         </div>
+//       ) : (
+//         <div>
+//           {props.missions.map(mission => (
+//             <div
+//               data-testid="mission-list"
+//               className="mission"
+//               key={mission.mission_id}
+//             >
+//               {mission.mission_name}
+//             </div>
+//           ))}
+//         </div>
+//       )}
+//     </section>
+//   );
+// };
+// export default MissionsList;
+
 const MissionsList = props => {
   return (
     <section className="missions-list">
@@ -8,7 +33,10 @@ const MissionsList = props => {
       ) : (
         <div>
           {props.missions.map(mission => (
-            <div className="mission" key={mission.mission_id}>
+            <div 
+              data-testid="mission-list" className="mission" 
+              key={mission.mission_id}
+              >
               {mission.mission_name}
             </div>
           ))}
